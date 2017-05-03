d91dfad438703e54d57d90bae10fa9e102e987b8
Teodor Bozhikov <Teodor.Bozhikov@progress.com>
2017-05-03 18:16:12 +0300

    Update dependencies to NS 3.0 and TS 2.2.2, related fixes

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,5 +1,7 @@
-import {Observable} from "data/observable";
-import Toolbox = require('nativescript-toolbox');
+import { Observable } from "data/observable";
+import * as Toolbox from 'nativescript-toolbox';
+import * as Moment from 'nativescript-toolbox/moment';
+
 
 function getMessage(counter) {
     if (counter <= 0) {
@@ -14,11 +16,16 @@ function createViewModel() {
     viewModel.counter = 42;
     viewModel.message = getMessage(viewModel.counter);
 
-    viewModel.onTap = function() {
+    viewModel.onTap = function () {
         this.counter--;
         this.set("message", getMessage(this.counter));
 
-        console.log('isDebug(): ' + Toolbox.isDebug());
+        // isDebug seems to be broken, commenting it out 
+        // console.log('isDebug(): ' + Toolbox.isDebug());
+    }
+
+    viewModel.onOpenWifiTap = function () {
+        console.log('openWifiSettings: ' + Toolbox.openWifiSettings());
     }
 
     return viewModel;
